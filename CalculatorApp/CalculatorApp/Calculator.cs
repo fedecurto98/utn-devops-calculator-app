933f0e3c953f60ce54456e61b63befa9ce7fc957
Federico Curto <fedecurto1@gmail.com>
2025-04-10 18:52:34 -0300

    add calculator logic

---
+++ CalculatorApp/CalculatorApp/Calculator.cs
@@ -0,0 +1,43 @@
+﻿namespace CalculatorApp
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Suma dos elementos
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Add(int a, int b) => a + b;
+
+        /// <summary>
+        /// Resta dos elementos
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Subtract(int a, int b) => a - b;
+
+        /// <summary>
+        /// Multiplica dos elementos
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Multiply(int a, int b) => a * b;
+
+        /// <summary>
+        /// Divide dos elementos
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Cannot divide by zero.");
+
+            return a / b;
+        }
+    }
+}
